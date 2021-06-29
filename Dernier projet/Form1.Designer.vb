c0bb91766a6d8841d50128a31462358d0da60b4e
novac <novac@DESKTOP-29TMLKB>
2021-06-29 10:22:21 -0400

    Ajoutez des fichiers projet.

---
+++ Dernier projet/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Output = New System.Windows.Forms.TextBox()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(18, 93)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(77, 75)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(125, 93)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(77, 75)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(231, 93)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(77, 75)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(18, 198)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(77, 75)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(125, 198)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(77, 75)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(231, 198)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(77, 75)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(18, 300)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(77, 75)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(125, 300)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(77, 75)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(231, 300)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(77, 75)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(18, 380)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(290, 41)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "0"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(333, 93)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(152, 75)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "/"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(333, 198)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(152, 75)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "*"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(333, 300)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(152, 66)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "-"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(333, 381)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(152, 40)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "+"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(680, 93)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(111, 180)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "CE"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button17.Location = New System.Drawing.Point(491, 88)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(153, 85)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Pour DIVISION" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Output
+        '
+        Me.Output.Location = New System.Drawing.Point(39, 34)
+        Me.Output.Name = "Output"
+        Me.Output.Size = New System.Drawing.Size(327, 26)
+        Me.Output.TabIndex = 17
+        Me.Output.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(18, 428)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(290, 40)
+        Me.Button18.TabIndex = 18
+        Me.Button18.Text = "."
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button16.Location = New System.Drawing.Point(491, 376)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(153, 85)
+        Me.Button16.TabIndex = 19
+        Me.Button16.Text = "=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Pour ADDITION" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button19.Location = New System.Drawing.Point(491, 276)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(153, 90)
+        Me.Button19.TabIndex = 20
+        Me.Button19.Text = "=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Pour SOUSTRACTION" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button20.Location = New System.Drawing.Point(491, 179)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(153, 91)
+        Me.Button20.TabIndex = 21
+        Me.Button20.Text = "=" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Pour MULTIPLICATION" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(803, 504)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Output)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Calculatrice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Output As TextBox
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+End Class
---
+++ Dernier projet/Form1.vb
@@ -0,0 +1,197 @@
+﻿Public Class Form1
+    Dim firstnumber As Decimal
+    Dim secondnumber As Decimal
+    Dim operations As Integer
+    Dim operatorSelect As Boolean = False
+    Private Sub Button_click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Output.Text <> "0" Then
+            Output.Text += "1"
+        Else
+            Output.Text = "1"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Output.Text <> "0" Then
+            Output.Text += "2"
+        Else
+            Output.Text = "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Output.Text <> "0" Then
+            Output.Text += "3"
+        Else
+            Output.Text = "3"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Output.Text <> "0" Then
+            Output.Text += "4"
+        Else
+            Output.Text = "4"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Output.Text <> "0" Then
+            Output.Text += "5"
+        Else
+            Output.Text = "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If Output.Text <> "0" Then
+            Output.Text += "6"
+        Else
+            Output.Text = "6"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If Output.Text <> "0" Then
+            Output.Text += "7"
+        Else
+            Output.Text = "7"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If Output.Text <> "0" Then
+            Output.Text += "8"
+        Else
+            Output.Text = "8"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If Output.Text <> "0" Then
+            Output.Text += "9"
+        Else
+            Output.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If Output.Text <> "0" Then
+            Output.Text += "0"
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        If Not (Output.Text.Contains(".")) Then
+            Output.Text += "."
+        End If
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Output.Text = "0"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        firstnumber = Output.Text
+        Output.Text = ""
+        operatorSelect = 1
+        operatorSelect = True
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        firstnumber = Output.Text
+        Output.Text = ""
+        operatorSelect = 2
+        operatorSelect = True
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        firstnumber = Output.Text
+        Output.Text = ""
+        operatorSelect = 3
+        operatorSelect = True
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        firstnumber = Output.Text
+        Output.Text = ""
+        operatorSelect = 4
+        operatorSelect = True
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If operatorSelect = True Then
+            secondnumber = Output.Text
+            If operations = 1 Then
+                Output.Text = firstnumber + secondnumber
+            ElseIf operations = 2 Then
+                Output.Text = firstnumber - secondnumber
+            ElseIf operations = 3 Then
+                Output.Text = firstnumber * secondnumber
+            ElseIf secondnumber = 0 Then
+                Output.Text = "erreur"
+                Else
+                Output.Text = firstnumber / secondnumber
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        If operatorSelect = True Then
+            secondnumber = Output.Text
+            If operations = 1 Then
+                Output.Text = firstnumber + secondnumber
+            ElseIf operations = 2 Then
+                Output.Text = firstnumber - secondnumber
+            ElseIf operations = 3 Then
+                Output.Text = firstnumber * secondnumber
+            ElseIf secondnumber = 0 Then
+                Output.Text = "erreur"
+            Else
+                Output.Text = firstnumber * secondnumber
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        If operatorSelect = True Then
+            secondnumber = Output.Text
+            If operations = 1 Then
+                Output.Text = firstnumber + secondnumber
+            ElseIf operations = 2 Then
+                Output.Text = firstnumber - secondnumber
+            ElseIf operations = 3 Then
+                Output.Text = firstnumber * secondnumber
+            ElseIf secondnumber = 0 Then
+                Output.Text = "erreur"
+            Else
+                Output.Text = firstnumber - secondnumber
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If operatorSelect = True Then
+            secondnumber = Output.Text
+            If operations = 1 Then
+                Output.Text = firstnumber + secondnumber
+            ElseIf operations = 2 Then
+                Output.Text = firstnumber - secondnumber
+            ElseIf operations = 3 Then
+                Output.Text = firstnumber * secondnumber
+            ElseIf secondnumber = 0 Then
+                Output.Text = "erreur"
+            Else
+                Output.Text = firstnumber + secondnumber
+
+            End If
+
+        End If
+    End Sub
+End Class
